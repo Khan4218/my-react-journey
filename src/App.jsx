3f4fb4bdc2123a0f732f75b2df0600ef64a7546f
Khan4218 <mirakber4@gmail.com>
2025-05-24 04:57:43 +0530

    Why React is declaractive than JavaScript

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,19 @@ const root = createRoot(document.getElementById("root"));  // yeah i did it
 
 function MyElement() {
   return <h1>if this runs you are learning well</h1>
-}
-
-root.render(
+}                                                         
+root.render(                                  //This code is using React
   <MyElement />
-)*/
+)
+*/
+/*
+const root = document.getElementById('root');
+const h1 = document.createElement('h1');          //This code is using javaScript
+h1.textContent = 'i am a good learner';
+root.appendChild(h1);*/
+
+
+const newRoot = document.getElementById('root');
+const h1 = document.createElement('h1');
+h1.textContent = 'Hello, vanilla JavaScript!'
+newRoot.appendChild(h1)
