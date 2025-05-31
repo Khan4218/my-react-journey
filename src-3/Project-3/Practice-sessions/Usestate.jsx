d11ed5bc27b340e7f0c154b4c320bc503a875383
Khan4218 <mirakber4@gmail.com>
2025-05-31 16:11:44 +0530

    use a function with a event handle in Usestate

--- src-3/Project-3/Practice-sessions/Usestate.jsx
+++ src-3/Project-3/Practice-sessions/Usestate.jsx
@@ -14,16 +14,24 @@
 
 //Use state array destructuring
 import React from "react"
-
+/**
+ * Challenge: 
+ * 1. Create a function called `handleClick` that runs
+ *    setIsImportant("Definitely")
+ * 2. Add a click event listener to the button
+ *    that runs `handleClick` when the button is clicked.
+ */
 export default function App() {
-  const [result, func] = React.useState("Yes")
-  console.log(result);
+  let [isImportant, setIsImportant] = React.useState("Yes")
+  function handleClick() {
+    setIsImportant("Definitely")
+  }
 
   return (
     <main>
       <h1 className="title">Is state important to know?</h1>
       {/* //here we dont need to give a index to result */}
-      <button className="value">{result}</button>
+      <button className="value" onClick={handleClick}>{isImportant}</button>
     </main>
   )
 }
