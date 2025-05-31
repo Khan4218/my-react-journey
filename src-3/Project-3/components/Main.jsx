de891de48b7a49e23ae1702fc91973ba9d107c42
Khan4218 <mirakber4@gmail.com>
2025-06-01 00:52:01 +0530

    Applied arrays useState in chef claude project

--- src-3/Project-3/components/Main.jsx
+++ src-3/Project-3/components/Main.jsx
@@ -1,27 +1,28 @@
+
+import React from "react";
 export default function Main() {
-  const ingredients = ['chicken', 'all the main spices', 'corn',]
+  const [ingredients, setIngredients] = React.useState([])
   const ingredientsListItems = ingredients.map((ingredient) => {
     return <li key={ingredient}>{ingredient}</li>
   })
-
-
   function handleOnSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget)
     const newIngredient = formData.get("ingredient")
-    ingredients.push(newIngredient);
-    console.log(ingredients)
+    setIngredients((prevIngredients) => {
+      return [...prevIngredients, newIngredient]
+    })
   }
-
   return (
     <main>
       <form onSubmit={handleOnSubmit} className="add-ingredient-form">
         <input className="input" placeholder="e.g. oregano" aria-label="Add ingredient" name="ingredient" />
-        <button className="button">+ Add ingredient </button>
+        <button className="button" >+ Add ingredient </button>
       </form>
       <ul>
         {ingredientsListItems}
       </ul>
     </main>
   )
+
 }
