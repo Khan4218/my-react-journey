b16b929938408d0e2066eb4753eb40156f4dbf6d
Khan4218 <mirakber4@gmail.com>
2025-06-01 17:26:21 +0530

    implemented form action to main project

--- src-3/Project-3/components/Main.jsx
+++ src-3/Project-3/components/Main.jsx
@@ -5,9 +5,7 @@ export default function Main() {
   const ingredientsListItems = ingredients.map((ingredient) => {
     return <li key={ingredient}>{ingredient}</li>
   })
-  function handleOnSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget)
+  function handleOnSubmit(formData) {
     const newIngredient = formData.get("ingredient")
     setIngredients((prevIngredients) => {
       return [...prevIngredients, newIngredient]
@@ -15,7 +13,7 @@ export default function Main() {
   }
   return (
     <main>
-      <form onSubmit={handleOnSubmit} className="add-ingredient-form">
+      <form action={handleOnSubmit} className="add-ingredient-form">
         <input className="input" placeholder="e.g. oregano" aria-label="Add ingredient" name="ingredient" />
         <button className="button" >+ Add ingredient </button>
       </form>
