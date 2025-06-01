c4e7784c28b81058c23e2439ce318e86f4e0ceb6
Khan4218 <mirakber4@gmail.com>
2025-06-01 17:17:18 +0530

    Use of form action by react 19

--- src-3/Project-3/Practice-sessions/forms.jsx
+++ src-3/Project-3/Practice-sessions/forms.jsx
@@ -1,17 +1,53 @@
 
+// export default function App() {
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     const formEl = event.currentTarget
+//     const formData = new FormData(formEl)
+//     const email = formData.get("email")
+//     // Gather the info from the form
+//     // Submit it to a backend
+//     formEl.reset();
+//   }
+//   return (
+//     <section>
+//       <form onSubmit={handleSubmit} method="post">
+//         <h1>Signup form</h1>
+//         <label htmlFor="email" >Email:</label>
+//         <input id="email" type="email" name="email" placeholder="example@email.com" />
+//         <br />
+//         <label htmlFor="password">Password:</label>
+//         <input id="password" type="password" name="password" />
+//         <br />
+//         <button>Submit</button>
+//       </form>
+//     </section>
+
+//   )
+// }
+
+// with form action the form submission is simple
 export default function App() {
-  function handleSubmit(event) {
-    event.preventDefault();
-    const formEl = event.currentTarget
-    const formData = new FormData(formEl)
+  function signUp(formData) {
     const email = formData.get("email")
     // Gather the info from the form
-    // Submit it to a backend
-    formEl.reset();
+    // Submit it to a backend 
+    /**
+   * Challenge: get the password from the form too and log
+   * it to the console to be sure it came in correctly.
+   */
+    const password = formData.get("password")
+    console.log(email)
+    console.log(password);
+
   }
+  /**
+   * Challenge: get the password from the form too and log
+   * it to the console to be sure it came in correctly.
+   */
   return (
     <section>
-      <form onSubmit={handleSubmit} method="post">
+      <form action={signUp} method="post">
         <h1>Signup form</h1>
         <label htmlFor="email" >Email:</label>
         <input id="email" type="email" name="email" placeholder="example@email.com" />
