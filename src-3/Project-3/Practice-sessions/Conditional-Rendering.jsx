bb3917ef98fbe86be526ecc46ac87cd28ad3abab
Khan4218 <mirakber4@gmail.com>
2025-06-04 00:53:42 +0530

    Practice of conditional rendering &&

--- src-3/Project-3/Practice-sessions/Conditional-Rendering.jsx
+++ src-3/Project-3/Practice-sessions/Conditional-Rendering.jsx
@@ -1,55 +1,72 @@
+// import React from "react"
+// export default function App() {
+//   return (
+//     <>
+//       <Jokes
+//         setup="I got my daughter a fridge for her birthday."
+//         punchline="I can't wait to see her face light up when she opens it."
+//         Upvotes={10} // non string and it is a number to props
+//         isPun={true} // adding boolean value to props
+//         comments={[{ author: "", text: "", title: "" }]} // adding array to props
+//       />
+//       <Jokes
+//         setup="How did the hacker escape the police?"
+//         punchline="He just ransomware!" />
+//       <Jokes
+//         setup="Why don't pirates travel on mountain roads?"
+//         punchline="Scurvy."
+//       />
+//       <Jokes
+//         setup="Why do bees stay in the hive in the winter?"
+//         punchline="Swarm."
+//       />
+//       <Jokes
+//         setup="What's the best thing about Switzerland?"
+//         punchline="I don't know, but the flag is a big plus!"
+
+//       />
+
+
+//     </>
+//   )
+// }
+
+
+
+// function Jokes(props) {
+//   const [isShown, setIsShown] = React.useState(false)
+
+//   function toggleShown() {
+//     setIsShown(prevShown => !prevShown)
+//   }
+
+//   console.log(isShown)
+//   return (
+
+//     <div>
+//       {props.setup && <h3>{props.setup}</h3>}
+//       {/* <p>{isShown ? props.punchline : false}</p> */}
+//       {isShown && <p>{props.punchline}</p>} {/*Using conditional rendering*/}
+//       <button onClick={toggleShown}>Show punchline</button>
+//       <hr />
+//     </div>
+//   )
+// }
+
 import React from "react"
+
 export default function App() {
+  const [unreadMessages, setUnreadMessages] = React.useState([])
+
+  /**
+   * Challenge:
+   * Only display the <h1> below if there are unread messages
+   */
+
   return (
-    <>
-      <Jokes
-        setup="I got my daughter a fridge for her birthday."
-        punchline="I can't wait to see her face light up when she opens it."
-        Upvotes={10} // non string and it is a number to props
-        isPun={true} // adding boolean value to props
-        comments={[{ author: "", text: "", title: "" }]} // adding array to props
-      />
-      <Jokes
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware!" />
-      <Jokes
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy."
-      />
-      <Jokes
-        setup="Why do bees stay in the hive in the winter?"
-        punchline="Swarm."
-      />
-      <Jokes
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-
-      />
-
-
-    </>
-  )
-}
-
-
-
-function Jokes(props) {
-  const [isShown, setIsShown] = React.useState(false)
-
-  function toggleShown() {
-    setIsShown(prevShown => !prevShown)
-  }
-
-  console.log(isShown)
-  return (
-
     <div>
-      {props.setup && <h3>{props.setup}</h3>}
-      {/* <p>{isShown ? props.punchline : false}</p> */}
-      {isShown && <p>{props.punchline}</p>} {/*Using conditional rendering*/}
-      <button onClick={toggleShown}>Show punchline</button>
-      <hr />
+      {unreadMessages.length > 0 && <h1>You have {unreadMessages.length} unread messages!</h1>}
+      {unreadMessages.length <= 0 && <h1>You have {unreadMessages.length} unread messages!</h1>}
     </div>
   )
 }
-
