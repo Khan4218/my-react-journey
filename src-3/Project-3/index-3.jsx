4844c1cdeb3ccb7f8dd40e8a0353cab445775f42
Khan4218 <mirakber4@gmail.com>
2025-06-03 16:47:48 +0530

    Conditional redering

--- src-3/Project-3/index-3.jsx
+++ src-3/Project-3/index-3.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client"
-import App from "./Practice-sessions/forms.jsx"
+import App from "./Practice-sessions/Conditional-Rendering"
+// import App from "./Practice-sessions/forms.jsx"
 // import App from "./App-3.jsx";
 // import App from "./Practice-sessions/Usestate.jsx";
 // import App from "./Practice-sessions/add-event-listenser.jsx"
