22700bc6eccfee03e2fef39accbf98e921ed2192
Khan4218 <mirakber4@gmail.com>
2025-06-05 01:22:37 +0530

    Sound pads challenge part 4.1 - local state

--- src-3/Project-3/Practice-sessions/Pad.jsx
+++ src-3/Project-3/Practice-sessions/Pad.jsx
@@ -1,13 +1,27 @@
+import React from "react"
 export default function Pad(props) {
+
   /**
-   * Challenge part 3:
-   * Our buttons got turned off by default! Update the code
-   * so if the button is "on", it has the className of "on".
-   */
+ * Challenge: Create state controlling whether
+ * this pad is "on" or "off". Use the incoming
+ * `props.on` to determine the initial state.
+ * 
+ * Create an event listener so when the pad is clicked,
+ * it toggles from "on" to "off".
+ * 
+ * Goal: clicking each pad should toggle it on and off.
+ */
+
+  const [padOn, setPadOn] = React.useState(props.on)
+  function handleClick() {
+    setPadOn(prevOn => !prevOn)
+  }
+
   return (
     <button
       style={{ backgroundColor: props.color }}
-      className={props.on ? "on" : undefined}
+      className={padOn ? "on" : undefined}
+      onClick={handleClick}
     ></button>
 
 
