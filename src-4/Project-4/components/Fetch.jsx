aa5974580c03bf56d2f265eacf0348c376e1be78
Khan4218 <mirakber4@gmail.com>
2025-06-05 22:58:50 +0530

    useEffect practice!

--- src-4/Project-4/components/Fetch.jsx
+++ src-4/Project-4/components/Fetch.jsx
@@ -53,28 +53,66 @@
 // }
 
 
+// import React from "react"
+
+// export default function Fetch() {
+//   const [starWarsData, setStarWarsData] = React.useState({})
+//   const [count, setCount] = React.useState(0)
+
+//   console.log("Rendered!")
+
+//   /**
+//    * Challenge: re-write the useEffect
+//    * It should run any time `count` changes
+//    * For now, just console.log("Effect function ran")
+//    */
+//   React.useEffect(function () {
+//     return console.log("Effect function ran");
+
+//   }, [])// it will check the empty array [] =>  [] so it will render once
+
+//   return (
+//     <div>
+//       <h2>The count is {count}</h2>
+//       <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+//       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+//     </div>
+//   )
+// }
+
 import React from "react"
 
 export default function Fetch() {
   const [starWarsData, setStarWarsData] = React.useState({})
-  const [count, setCount] = React.useState(0)
-
-  console.log("Rendered!")
+  const [count, setCount] = React.useState(1)
 
   /**
-   * Challenge: re-write the useEffect
-   * It should run any time `count` changes
-   * For now, just console.log("Effect function ran")
+   * Challenge part 1:
+   * Fetch the data from this url: "https://swapi.dev/api/people/1"
+   * and save it in the starWarsData state. Make sure you don't
+   * get stuck in an infinite rendering loop!
    */
-  React.useEffect(function () {
-    return console.log("Effect function ran");
 
-  }, [])// it will check the empty array [] =>  [] so it will render once
+  /**
+     * Challenge part 2:
+     * Combine the "count" state with the request URL
+     * so that pressing the "Get next character" button
+     * will get a new character from the Star Wars API.
+     * Remember to consider the dependencies array!
+     */
+  React.useEffect(function () {
+    fetch(`https://swapi.tech/api/people/${count}`)
+      .then(res => res.json())
+      .then(data => setStarWarsData(data))
+  }, [count])//changes count in Api when re renders
+
+  console.log(count);
+
 
   return (
     <div>
       <h2>The count is {count}</h2>
-      <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>Get next character</button>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   )
