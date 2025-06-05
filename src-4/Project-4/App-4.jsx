c9247efc31e0ab3b54bb9ad3e9f71dc578f1117d
Khan4218 <mirakber4@gmail.com>
2025-06-05 23:27:31 +0530

    Meme Generator - Fetch Memes

--- src-4/Project-4/App-4.jsx
+++ src-4/Project-4/App-4.jsx
@@ -1,14 +1,14 @@
-// import Header from "./components/Header";
-// import Main from "./components/Main";
+import Header from "./components/Header";
+import Main from "./components/Main";
 import Fetch from "./components/Fetch";
 
 
 export default function App() {
   return (
     <>
-      {/* <Header />
-      <Main /> */}
-      <Fetch />
+      <Header />
+      <Main />
+      {/* <Fetch /> */}
     </>
   )
 }
--- src-4/Project-4/components/Main.jsx
+++ src-4/Project-4/components/Main.jsx
@@ -7,6 +7,29 @@ export default function Main() {
 
   })
 
+  const [allMemes, setAllMemes] = React.useState([])
+  /**
+   * Challenge:
+   * Get an array of memes from the imgflip API as soon as
+   * this component renders for the first time.
+   * Check the imgflip documentation for the correct URL.
+   * Save the array of memes (not the whole response
+   * data) to state. (For this app, we'll randomly choose
+   * one of the memes from this array when the user clicks
+   * the "Get a new meme image" button, but we'll do that in
+   * a separate challenge.)
+   * 
+   * Hint: for now, don't try to use an async/await function.
+   * Instead, use `.then()` to resolve the promises
+   * from using `fetch`. We'll learn why after this challenge.
+   */
+  React.useEffect(function () {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(res => res.json())
+      .then(data => setAllMemes(data.data.memes))
+  }, [])
+
+
   function handleChange(event) {
     const { name, value } = event.currentTarget
     console.log(name);
