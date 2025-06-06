ea7c0b8259f151665e9fc8db849c0e1bbfb0bbc0
Khan4218 <mirakber4@gmail.com>
2025-06-07 00:42:28 +0530

    state and Effect practice

---
+++ src-4/Project-4/Practice-sessions/State-and-Effect-Practice.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect, } from "react"
+export default function App() {
+  /**
+   * Challenge:
+   * 1. Create state called `show`, default to `true`
+   * 2. When the button is clicked, toggle `show`
+   * 3. Only display `<WindowTracker>` if `show` is `true`
+   */
+  const [show, setShow] = useState(true)
+
+
+  function handleClick() {
+    setShow(prevValue => !prevValue)
+
+
+  }
+  return (
+    <main className="container">
+      <button onClick={handleClick}>
+        Toggle WindowTracker
+      </button>
+      {show ? <WindowTracker /> : null}
+    </main>
+  )
+}
+
+
+
+
+function WindowTracker() {
+  /**
+   * Challenge:
+   * 1. Create state called `windowWidth`, default to 
+   *    `window.innerWidth`
+   * 2. When the window width changes, update the state
+   * 3. Display the window width in the h1 so it updates
+   *    every time it changes
+   */
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  console.log(windowWidth);
+
+  useEffect(() => {
+
+    window.addEventListener("resize", function handleResize() {
+      setWindowWidth(window.innerWidth)
+    })
+  }, [])
+
+
+  return (
+    <h1>Window width: {windowWidth}</h1>
+  )
+}
--- src-4/Project-4/index-4.jsx
+++ src-4/Project-4/index-4.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client"
-import App from "./App-4";
+// import App from "./App-4";
+import App from "./Practice-sessions/State-and-Effect-Practice";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
