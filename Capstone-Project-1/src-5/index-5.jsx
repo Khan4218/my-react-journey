6b9c2591e96907a7432712670d12cac6a3727774
Khan4218 <mirakber4@gmail.com>
2025-06-13 01:33:29 +0530

    Capstone Project Tenzies

---
+++ Capstone-Project-1/src-5/index-5.jsx
@@ -0,0 +1,7 @@
+import App from "./App-5.jsx";
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <App />
+)
---
+++ Capstone-Project-1/src-5/main-5.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index-5.css'
+import App from './index-5.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
