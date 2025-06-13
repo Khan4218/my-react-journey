76f439e381b7761b4b5b20ec9b853bdd9b63a3d0
Khan4218 <mirakber4@gmail.com>
2025-06-14 04:38:48 +0530

    generate 10 random numbers

--- Capstone-Project-1/src-5/App-5.jsx
+++ Capstone-Project-1/src-5/App-5.jsx
@@ -1,5 +1,38 @@
-import Main from "./components/Main.jsx"
+import Die from "./components/Die";
+
 
 export default function App() {
-  return <Main />
+  function generateAllNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      const rand = Math.ceil(Math.random() * 6)
+      newDice.push(rand);
+    }
+
+    return newDice
+  }
+
+  console.log(generateAllNewDice());
+
+
+
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
+
+    </main>
+  )
+
+
 }
